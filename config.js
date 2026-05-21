fd945cd56654b7007267e76705dc96a3c023b97d
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-21 14:53:51 +0300

    Update DATA_URL to point to live Google Apps Script

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Paste your Google Apps Script Web App URL below.
 // Keep it empty while testing locally; the website will use sample-data.js.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbzldJmZYiKAScQWA3Xu5SFjQhYhyFmMJf6SIaDA3LL9d0k1nwhwmfbpi8qkq5RrTv0lyw/exec",
   APPROVED_ONLY: true,
   SHOW_INTERNAL_NOTES: false,
   CURRENCY: "QAR"
