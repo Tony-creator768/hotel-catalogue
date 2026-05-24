e5c9c3388ea2e740c3c3ff785732877b706d5593
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-24 11:36:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Paste your Google Apps Script Web App URL below.
 // Keep it empty while testing locally; the website will use sample-data.js.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
   APPROVED_ONLY: true,
   SHOW_INTERNAL_NOTES: false,
   CURRENCY: "QAR"
