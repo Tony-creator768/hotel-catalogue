cb084dc4d2e00a97aac3ceb6f9ad4c09d554c3fc
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-25 11:29:59 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Paste your Google Apps Script Web App URL below.
-// Keep it empty while testing locally; the website will use sample-data.js.
-window.CATALOGUE_CONFIG = {
-  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
-  APPROVED_ONLY: true,
-  SHOW_INTERNAL_NOTES: false,
-  CURRENCY: "QAR"
-};
