a5a1d7c1cdcf70d091835f43dd4bc059b1d3ab51
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-25 11:33:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Paste your deployed Google Apps Script Web App URL below when you are ready for live Google Sheets data.
 // Leave blank to use the bundled sample-data.js file.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "",
+  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
   APPROVED_ONLY: true
 };
