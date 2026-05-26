56d18a8638f3c30e5271e5ca79cc2ff47bd06dde
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-26 14:58:55 +0300

    Delete config.example.js

--- config.example.js
+++
@@ -1,7 +0,0 @@
-// Copy this file to config.js if you ever need to reconnect the website to a different Apps Script deployment.
-window.CATALOGUE_CONFIG = {
-  DATA_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
-  APPROVED_ONLY: true,
-  SHOW_INTERNAL_NOTES: false,
-  CURRENCY: "QAR"
-};
