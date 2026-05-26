6be3075abe5dac88c54fd91a4c8cfeb90b602a6f
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-26 15:29:18 +0300

    Update DATA_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Paste your deployed Google Apps Script Web App URL below when you are ready for live Google Sheets data.
 // Leave blank to use the bundled sample-data.js file.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
+  DATA_URL: "https://script.google.com/macros/s/AKfycbyx_b48WfqZjtOWe7rUHci9zOrk_1a1uAxHpo1zjb62oDXHlNIx-XCqKGKpXN44nVi2/exec",
   APPROVED_ONLY: true
 };
