ca7dd7f6c86a2016b05de16fb96796a63c1af53c
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-26 15:15:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Paste your Google Apps Script Web App URL below.
 // This package keeps your current URL. If the site does not load live data, redeploy Code.gs and paste the new /exec URL here.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
+  DATA_URL: "",
   APPROVED_ONLY: true,
   SHOW_INTERNAL_NOTES: false,
   CURRENCY: "QAR"
