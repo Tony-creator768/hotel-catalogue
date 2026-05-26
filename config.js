c7158dd44e69518f90d581ad4c0f6d87412557e4
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-26 14:58:40 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Paste your Google Apps Script Web App URL below.
-// This file already contains your current Apps Script URL.
-// Keep APPROVED_ONLY true so only rows marked Approved appear online.
-window.CATALOGUE_CONFIG = {
-  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
-  APPROVED_ONLY: true,
-  SHOW_INTERNAL_NOTES: false,
-  CURRENCY: "QAR"
-};
